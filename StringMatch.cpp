f796f9408650f082dd19fdfbded06fda1cd91bc8
훈 서 <syhu2001@live.co.kr>
2021-07-12 13:51:44 +0900

    20210712

---
+++ StringMatch.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int strStr(string haystack, string needle) {
+    int len = needle.size();
+    if (haystack.size() < len) return -1;
+    for (int idx = 0; idx <= haystack.size() - len; idx++) {
+        if (string(haystack.begin() + idx, haystack.begin() + idx + len) == needle) return idx;
+    }
+    return -1;
+}
